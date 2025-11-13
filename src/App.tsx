346030d4f97633718efc7f11de59322a050bf336
scott-mescudi <anish.chittu2007@gmail.com>
2025-11-13 01:30:19 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import type { Component } from "solid-js";
+import EntryCard from "./components/EntryCard";
+
+export default function App() {
+  return (
+    <>
+      <div class="flex flex-col gap-5">
+        <p class="text-text text-sm mb-5">Side Projects</p>
+        <EntryCard
+          description="Agentic marketing platform. From competitor analysis to publishing winning ads, Revala's agent does it all."
+          title="Revala"
+          tags={["Go", "Postgres", "Solidjs", "Weviate"]}
+          date="2025"
+          projectUrl="https://www.revala.io"
+        />
+        <EntryCard
+          description="The fastest steganography library in the golang ecosystem. Features include variable bit encoding, audio steganography, reed solomon error correction etc."
+          title="Stegano"
+          tags={["Go", "Library"]}
+          date="2024"
+          projectUrl="https://github.com/501urchin/stegano"
+        />
+      </div>
+    </>
+  );
+}
---
+++ src/components/EntryCard.tsx
@@ -0,0 +1,74 @@
+import { For, Show } from "solid-js";
+
+interface Props {
+  date: string;
+  title: string;
+  description: string;
+  
+  projectUrl?: string;
+  tags?: string[];
+  iconUrl?: string;
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-move-up-right"
+    >
+      <path d="M13 5H19V11" />
+      <path d="M19 5L5 19" />
+    </svg>
+  );
+}
+
+export default function EntryCard(p: Props) {
+  const hasLink = () => p.projectUrl && p.projectUrl.trim() !== "";
+  const hasIcon = () => p.iconUrl && p.iconUrl.trim() !== "";
+
+  return (
+    <div class="flex flex-row gap-2 text-sm antialiased">
+      <h1 class="text-text-muted-extra w-[12ch] text-start">{p.date}</h1>
+
+      <div class="flex flex-col w-[43ch] gap-1">
+        <Show when={hasIcon()}>
+          <div class="flex items-center justify-center size-10"></div>
+        </Show>
+        <Show when={hasLink()} fallback={<h2 class="text-text">{p.title}</h2>}>
+          <a
+            href={p.projectUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-text flex flex-row items-center gap-1 hover:underline duration-300 cursor-pointer ease-in-out"
+          >
+            {p.title}
+            <ArrowIcon />
+          </a>
+        </Show>
+
+        <p class="text-text-muted">{p.description}</p>
+
+        <div class="overflow-clip flex flex-row gap-1">
+          <For each={p.tags}>
+            {(t, i) => (
+              <>
+                <div class="text-text-muted-extra">{t}</div>
+                {i() + 1 === p.tags?.length ? null : (
+                  <div class="text-text-muted-extra">•</div>
+                )}
+              </>
+            )}
+          </For>
+        </div>
+      </div>
+    </div>
+  );
+}
