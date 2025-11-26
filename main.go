2b234ed339a43ef5ba2492d03c4011f05eff1b6a
501urchin <jaychhitu@gmail.com>
2025-11-26 23:46:42 +0100

    Refactor file serving to use embedded filesystem for static assets

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+
+	_ "embed"
 
 	"github.com/a-h/templ"
 )
 
+//go:embed public/**
+var publicFolderEmbed embed.FS
+
 func main() {
-    fs := http.FileServer(http.Dir("public"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	publicFs, err := fs.Sub(publicFolderEmbed, "public")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    http.Handle("/", templ.Handler(App()))
+	fs := http.FileServer(http.FS(publicFs))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    fmt.Println("Listening on http://localhost:3000")
-    err := http.ListenAndServe(":3000", nil)
-    if err != nil {
-        log.Fatalln("failed to start portfolio: ", err.Error())
-    }
+	http.Handle("/", templ.Handler(App()))
+
+	fmt.Println("Listening on http://localhost:3000")
+	err = http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatalln("failed to start portfolio: ", err.Error())
+	}
 }
